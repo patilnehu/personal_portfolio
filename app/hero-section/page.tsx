cad560724f40562b1384d19609e4483661a162cb
patilnehu <dev.nehapatil@gmail.com>
2025-08-01 16:47:36 +0530

    added changes

--- app/hero-section/page.tsx
+++ app/hero-section/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Download, ArrowDown } from "lucide-react";
+import { Download } from "lucide-react";
 import Link from "next/link";
 import { motion } from "framer-motion";
 
